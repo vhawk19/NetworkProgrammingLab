080c48921afb31d35eea8ca0d962abc04221876e
Varun Krishna S <varunhawk19@gmail.com>
2021-04-07 23:30:53 +0530

    fix the syscalls

--- cycle_1/sys_calls.go
+++ cycle_1/sys_calls.go
@@ -7,11 +7,11 @@ import (
 
 func main() {
 	fmt.Printf("The current processs id is %d\n", syscall.Getpid())
-	fmt.Printf("The parent processs id is %d\n", syscall.Getpid())
-	fmt.Printf("The real user id is %d\n", syscall.Getpid())
-	fmt.Printf("The group id is %d\n", syscall.Getpid())
-	fmt.Printf("The effective user id is %d\n", syscall.Getpid())
-	fmt.Printf("The effective group id is %d\n", syscall.Getpid())
+	fmt.Printf("The parent processs id is %d\n", syscall.Getppid())
+	fmt.Printf("The real user id is %d\n", syscall.Getuid())
+	fmt.Printf("The group id is %d\n", syscall.Getgid())
+	fmt.Printf("The effective user id is %d\n", syscall.Geteuid())
+	fmt.Printf("The effective group id is %d\n", syscall.Getegid())
 }
 
 
